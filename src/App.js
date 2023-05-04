5d93176162b2a5e2e1c88b4c5d78030839a4bc8e
Sai Prashant Saxena <saiprashant.saxena@gmail.com>
2023-05-04 22:12:04 +0530

    [add] : basic  structure added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import Dashboard from "./components/Dashboard/Dashboard";
+import Sidebar from "./components/Sidebar/Sidebar";
+
 function App() {
-  return <div className="text-center">Hi</div>;
+  return (
+    <div className="grid-cols-11 grid">
+      <div className="col-span-3">
+        <Sidebar />
+      </div>
+      <div className="col-span-8">
+        <Dashboard />
+      </div>
+    </div>
+  );
 }
 
 export default App;
